dc8b4ed91a1677ea3d54d2e0eb7008cc9e22053c
Robert Riekhof <RobertBRiekhof@gmail.com>
2020-02-01 20:47:15 -0600

    Updates

---
+++ precache-manifest.a26813ce57c4e3c37a9894cb5399b235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e2f9284a06e22bbac926d5e6b04384",
+    "url": "/Super-Clicky-Game/index.html"
+  },
+  {
+    "revision": "4a439d425e64b7940a4b",
+    "url": "/Super-Clicky-Game/static/css/main.7e28413c.chunk.css"
+  },
+  {
+    "revision": "0175b0c4096e237f7d39",
+    "url": "/Super-Clicky-Game/static/js/2.d3a3dd99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Super-Clicky-Game/static/js/2.d3a3dd99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a439d425e64b7940a4b",
+    "url": "/Super-Clicky-Game/static/js/main.00b8af96.chunk.js"
+  },
+  {
+    "revision": "9e3e5fa54cb3c4f1f8d4",
+    "url": "/Super-Clicky-Game/static/js/runtime-main.cfaadda9.js"
+  }
+]);
